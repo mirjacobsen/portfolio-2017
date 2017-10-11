dd5b7c174a0b8768e705e316bf80812323793c9b
Miranda Jacobsen <mjacobsen@ssccaml0267.lululemoninternal.com>
2017-10-11 16:27:15 -0700

    add base styles for form - actual design still needed

--- my-app/src/components/AddProjectForm/AddProjectForm.js
+++ my-app/src/components/AddProjectForm/AddProjectForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './add-project-form.css';
 import fire from '../../fire';
-// import firebase from 'firebase';
 import FileUploader from 'react-firebase-file-uploader';
 
 class AddProjectForm extends Component {
@@ -58,48 +57,59 @@ class AddProjectForm extends Component {
   };
 
   //TODO: addProject Form only shows up if I'm logged in
-
   render() {
     return (
       <form
         ref={input => (this.projectForm = input)}
-        className="project"
+        className="project-form"
         onSubmit={e => this.createProject(e)}
       >
+        <h3>Add Project</h3>
         <input
           type="text"
           name="projectTitle"
           placeholder="Project Title"
+          className="project-form__input"
           ref={input => (this.title = input)}
         />
         <input
           type="text"
           name="projectUrl"
           placeholder="Project Url"
+          className="project-form__input"
           ref={input => (this.url = input)}
         />
         <input
           type="text"
           name="projectTechnologies"
           placeholder="Project Technologies"
+          className="project-form__input"
           ref={input => (this.technologies = input)}
         />
         <input
           type="text"
           name="projectDate"
           placeholder="Project Date"
+          className="project-form__input"
           ref={input => (this.date = input)}
         />
-        <FileUploader
-          accept="image/*"
-          name="projectImage"
-          storageRef={fire.storage().ref('images')}
-          onUploadStart={this.handleUploadStart}
-          onUploadError={this.handleUploadError}
-          onUploadSuccess={this.handleUploadSuccess}
-          onProgress={this.handleProgress}
-        />
-        <button type="submit"> + Add Project</button>
+        <label className="project-form__file-upload">
+          Select image
+          <FileUploader
+            hidden
+            accept="image/*"
+            name="projectImage"
+            storageRef={fire.storage().ref('images')}
+            onUploadStart={this.handleUploadStart}
+            onUploadError={this.handleUploadError}
+            onUploadSuccess={this.handleUploadSuccess}
+            onProgress={this.handleProgress}
+          />
+        </label>
+        <button type="submit" className="project-form__submit">
+          {' '}
+          + Add Project
+        </button>
       </form>
     );
   }
